7d682f7f11c1c3a3525b6949bf7837979182c9d1
Rubén <74114026+rubzip@users.noreply.github.com>
2026-01-15 10:17:26 +0100

    Update kinematics.py

--- src/kinematics.py
+++ src/kinematics.py
@@ -14,13 +14,14 @@ class BykeKinematics:
         # 1. First derivative
         x_dot = current_state.v * np.cos(current_state.theta)
         y_dot = current_state.v * np.sin(current_state.theta)
-        theta_dot = (current_state.v / self.length) * np.tan(phi)
         v_dot = a
+        theta_dot = (current_state.v / self.length) * np.tan(phi)
 
         # 2. Update state
         new_x = current_state.x + x_dot * self.delta_t
         new_y = current_state.y + y_dot * self.delta_t
-        new_theta = current_state.theta + theta_dot * self.delta_t
         new_v = current_state.v + v_dot * self.delta_t
-
+        new_theta = current_state.theta + theta_dot * self.delta_t
+        new_theta = new_theta % (2 * np.pi)
+        
         return CarState(x=new_x, y=new_y, theta=new_theta, v=new_v)
