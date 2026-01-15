86d82a499c001fa05c52fd126796d74c9139ffb7
Rubén <74114026+rubzip@users.noreply.github.com>
2026-01-15 10:12:24 +0100

    Update kinematics.py

--- src/kinematics.py
+++ src/kinematics.py
@@ -1,22 +1,26 @@
 import numpy as np
-
+from .models import CarState
 
 class BykeKinematics:
     def __init__(self, length: float, delta_t: float = 1e-2):
+        if length <= 0:
+            raise ValueError(f"`length` should be a positive number. Given value: {length}")
+        if delta_t <= 0:
+            raise ValueError(f"`delta_t` should be a positive number. Given value: {delta_t}")
         self.length = length # Distance between wheels
-        self.delta_t = delta_t
+        self.delta_t = delta_t # Time Step
     
-    def update(self, x: float, y: float, v: float, theta: float, a: float, phi: float) -> tuple[float, float, float, float]:
+    def update(self, current_state: CarState, a: float, phi: float) -> CarState:
         # 1. First derivative
-        x_dot = v * np.cos(theta)
-        y_dot = v * np.sin(theta)
-        theta_dot = (v / self.length) * np.tan(phi)
+        x_dot = current_state.v * np.cos(current_state.theta)
+        y_dot = current_state.v * np.sin(current_state.theta)
+        theta_dot = (current_state.v / self.length) * np.tan(phi)
         v_dot = a
 
         # 2. Update state
-        new_x = x + x_dot * self.delta_t
-        new_y = y + y_dot * self.delta_t
-        new_theta = theta + theta_dot * self.delta_t
-        new_v = v + v_dot * self.delta_t
+        new_x = current_state.x + x_dot * self.delta_t
+        new_y = current_state.y + y_dot * self.delta_t
+        new_theta = current_state.theta + theta_dot * self.delta_t
+        new_v = current_state.v + v_dot * self.delta_t
 
-        return new_x, new_y, new_theta, new_v
+        return CarState(x=new_x, y=new_y, theta=new_theta, v=new_v)
